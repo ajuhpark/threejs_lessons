b98afe21ab34156439fd66af7a1ddfcd440d6c33
Andrew Park <ajuhpark@gmail.com>
2024-02-20 18:07:57 -0800

    recreating some cubes for index_11.html

--- 04-transform-objects/exercise/src/home_2/script_11.js
+++ 04-transform-objects/exercise/src/home_2/script_11.js
@@ -2,6 +2,12 @@ import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 
 /**
+- Create 3 Meshes composed of 3 different geometries (sphere, plane, torus)
+- use the same MeshBasicMaterial on all 3
+- Move the sphere on the left and the torus on the right
+*/
+
+/**
  * Base
  */
 // Canvas
@@ -9,6 +15,65 @@ const canvas = document.querySelector('canvas.webgl')
 
 // Scene
 const scene = new THREE.Scene()
+
+/**
+ *  Objects
+ */
+
+// Objects as a group
+
+const group = new THREE.Group()
+group.position.x = 1.5
+group.position.y = 1.5
+group.position.z = 1
+
+group.scale.y = 1
+group.rotation.y = 0
+
+scene.add(group)
+
+// Individual objects
+const geometry_1 = new THREE.BoxGeometry(1, 1, 1)
+const material_1 = new THREE.MeshBasicMaterial({ color: 0xff0000, wireframe: true })
+const mesh_1 = new THREE.Mesh(geometry_1, material_1)
+
+const cube1 = mesh_1
+
+cube1.position.x = 0
+cube1.position.y = 1
+cube1.position.z = 1
+
+group.add(cube1)
+
+const geometry_2 = new THREE.BoxGeometry(1, 1, 1)
+const material_2 = new THREE.MeshBasicMaterial({ color: 0X00ff00, wireframe: true })
+const mesh_2 = new THREE.Mesh(geometry_2, material_2)
+
+const cube2 = mesh_2
+
+cube2.position.x = 1
+cube2.position.y = 1
+cube2.position.z = 1
+
+cube2.position.x = - 2
+group.add(cube2)
+
+const geometry_3 = new THREE.BoxGeometry(1, 1, 1)
+const material_3 = new THREE.MeshBasicMaterial({ color: 0X0000ff, wireframe: true })
+const mesh_3 = new THREE.Mesh(geometry_3, material_3)
+
+const cube3 = mesh_3
+
+cube3.position.x = 1
+cube3.position.y = 1
+cube3.position.z = 1
+
+cube3.position.x = 2
+group.add(cube3)
+
+// Axes helper - shows the axes
+const axesHelper = new THREE.AxesHelper(2)
+scene.add(axesHelper)
 
 /**
  * Sizes
@@ -38,9 +103,9 @@ window.addEventListener('resize', () =>
  */
 // Base camera
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
-camera.position.x = 1
-camera.position.y = 1
-camera.position.z = 2
+camera.position.x = 3
+camera.position.y = 3
+camera.position.z = 9
 scene.add(camera)
 
 // Controls
